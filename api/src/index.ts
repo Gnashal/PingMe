5d527314bfb7102d9d2d775f2036feb3f89a7c9d
Gnashal <yousifceballos@gmail.com>
2025-01-11 19:15:37 +0800

    successfull connection to db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,28 @@
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import { MongoClient } from "mongodb";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const url: any = process.env.MONGO_URL;
+const db_client = new MongoClient(url);
+console.log(url);
+
+async function connectDb() {
+  try {
+    await db_client.connect()
+    console.log("Success");
+  } catch (err) {
+    console.log("Not success")
+    if (err) throw err;
+  }
+}
+
+connectDb();
+
+
+const app = new Elysia()
+.use(swagger())
+.get('/test', 'Hello from backend')
+.listen(4000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
