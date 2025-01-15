fa010c03a068b2d448bf63386ca56aec0bf400cd
Gnashal <yousifceballos@gmail.com>
2025-01-15 12:28:13 +0800

    login api integration v.1

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,19 +1,59 @@
-import { Link } from 'react-router-dom'
+import { data, Link, useNavigate } from 'react-router-dom'
 import logo from '../assets/logo.jpg'
 import './styles/login.css'
 import { useState } from 'react'
+import axios from 'axios'
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const [isSubmitted, setSubmitStatus] = useState(false);
 
-    function handleSubmit(e) {
+    const navigate = useNavigate();
+
+    async function handleLogin(user, pass) {
+        try {
+            const api_res = await axios.post('/login', {user, pass})
+        if (api_res.status === 200) {
+            setSubmitStatus(true);
+        } else {
+            setError('Unexpected response from the server.');
+            console.error(api_res);
+        }
+        } catch (err) {
+            console.error(err);
+            setError('Error contacting the database. Please try again later.');
+        }
+        
+    }
+
+    async function handleSubmit(e) {
         e.preventDefault();
         if (!username || !password) {
-            setError("Missing Fields!");
+        setError("Missing Fields!");
+        return;
+        }
+        try {
+           const response = await axios.post('/verify-user', {username, password})
+           if (response.status === 200) {
+            handleLogin(username, password)
+            setError('');
+            return;
+           } else {
+            setError('Your account does not exist!');
+            console.error(response);
+         }
+
+        } catch (err) {
+            console.error(err);
+            setError('Error contacting the database. Please try again later.');
         } 
     }
+
+        if (isSubmitted) {
+            navigate('/dashboard')
+        }
     return (
         <>
         <img src={logo} alt="PingMe Logo" className="logo" /> 
